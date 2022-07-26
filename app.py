2e771a9f7ab7fdb430d71c7c969f52516990fa2c
Benedani <18270989+Benedani@users.noreply.github.com>
2022-07-26 09:49:25 +0200

    add try except
    solves any exception problem instantly !

--- app.py
+++ app.py
@@ -28,22 +28,25 @@ def inference(model_inputs:dict) -> dict:
     global model
     global tokenizer
 
-    # Parse out your arguments
-    prompt = model_inputs.get('prompt', "")
-    length = model_inputs.get('length', 50)
-    temperature = model_inputs.get('temperature', 0.9)
-    top_p = model_inputs.get('topP', 0.9)
-    
-    # Tokenize inputs
-    input_tokens = tokenizer.encode(prompt, return_tensors="pt").to(device)
+    try:
+        # Parse out your arguments
+        prompt = model_inputs.get('prompt', "")
+        length = model_inputs.get('length', 50)
+        temperature = model_inputs.get('temperature', 0.9)
+        top_p = model_inputs.get('topP', 0.9)
 
-    # Run the model
-    output = model.generate(input_tokens, temperature=temperature, max_new_tokens=length, top_p=top_p, repetition_penalty=1.9, diversity_penalty=1.9)
+        # Tokenize inputs
+        input_tokens = tokenizer.encode(prompt, return_tensors="pt").to(device)
 
-    # Decode output tokens
-    output_text = tokenizer.batch_decode(output, skip_special_tokens = True)[0]
+        # Run the model
+        output = model.generate(input_tokens, temperature=temperature, max_new_tokens=length, top_p=top_p, repetition_penalty=1.9, diversity_penalty=1.9)
 
-    result = {"output": output_text}
+        # Decode output tokens
+        output_text = tokenizer.batch_decode(output, skip_special_tokens = True)[0]
 
-    # Return the results as a dictionary
-    return result
+        result = {"output": output_text}
+
+        # Return the results as a dictionary
+        return result
+    except Exception as skill_issue:
+        return {"skill_issue": skill_issue}

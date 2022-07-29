844da75704ac7eb94ae394a04a732de04cf6653e
Benedani <18270989+Benedani@users.noreply.github.com>
2022-07-29 20:13:29 +0200

    performance: steps system
    stop generating all of the tokens only to end up grabbing a single word worth of response!

--- app.py
+++ app.py
@@ -34,26 +34,39 @@ def inference(model_inputs:dict) -> dict:
     try:
         # Parse out your arguments
         prompt = model_inputs.get('prompt', "")
-        length = model_inputs.get('length', 50)
         temperature = model_inputs.get('temperature', 0.9)
         top_p = model_inputs.get('topP', 0.9)
         repetition_penalty = model_inputs.get('repetitionPenalty', 1.0)
         
+        # Funny steps system
+        # The bot generates `tokens_per_step` tokens, then checks whether it stopped generating text for the bot or not.
+        # This is repeated `steps` times
+        tokens_per_step = model_inputs.get('tokensPerStep', 10)
+        steps = model_inputs.get('steps', 5)
+        line_start = model_inputs.get('lineStart', "Phineas ")
+        
         if prompt == "KEEPALIVE" and length == 1:
             return {"output": "keeping alive"}
-
-        # Tokenize inputs
-        input_tokens = tokenizer.encode(prompt, return_tensors="pt").to(device)
         
         #bad_words_ids = [
         #    tokenizer.encode(bad_word, add_prefix_space=True) for bad_word in ["??", "???", "????", "?????", "??????", "???????", "????????", "anal", "arse", "ass", "bitch", "boner", "dick", "dildo", "nigga", "nigge", "penis", "pussy", "vagina"]
         #]
 
         # Run the model
-        output = model.generate(input_tokens, use_cache=True, do_sample=True, temperature=temperature, max_new_tokens=length, top_p=top_p, repetition_penalty=repetition_penalty)
+        current_tokens = tokenizer.encode(prompt, return_tensors="pt").to(device)
+        output_text = prompt
+        
+        for _i in range(steps):
+            current_tokens = model.generate(current_tokens, use_cache=True, do_sample=True, temperature=temperature, max_new_tokens=tokens_per_step, top_p=top_p, repetition_penalty=repetition_penalty)
 
-        # Decode output tokens
-        output_text = tokenizer.batch_decode(output, skip_special_tokens = True)[0]
+            # Decode output tokens
+            output_text = tokenizer.batch_decode(output, skip_special_tokens = True)[0]
+            
+            # Should we exit?
+            ln = output_text.splitlines()[-1]
+            if len(ln) >= len(line_start) and not ln.starts_with(line_start):
+                # ok it's generating something unrelated now
+                break
 
         result = {"output": output_text}
 

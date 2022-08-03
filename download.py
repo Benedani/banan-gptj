713841ef24221dd80b743561b52517cd8fe90d80
Benedani <18270989+Benedani@users.noreply.github.com>
2022-08-03 09:03:54 +0200

    remake build
    brr

--- download.py
+++ download.py
@@ -1,7 +1,7 @@
 # In this file, we define download_model
 # It runs during container build time to get model weights built into the container
 
-# In this example: A Huggingface GPTJ model
+# In this example: A Huggingface GPT NeoX model
 
 from transformers import GPTNeoXForCausalLM, GPTNeoXTokenizerFast
 import torch
